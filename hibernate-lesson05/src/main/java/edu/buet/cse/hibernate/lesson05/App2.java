61c662cf950bb410ddac7333aab76adbc6a373ba
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-03 21:17:39 +0600

    added a new example to check the correctness of one-to-many mapping

---
+++ hibernate-lesson05/src/main/java/edu/buet/cse/hibernate/lesson05/App2.java
@@ -0,0 +1,21 @@
+package edu.buet.cse.hibernate.lesson05;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import edu.buet.cse.hibernate.lesson05.model.Comment;
+import edu.buet.cse.hibernate.lesson05.util.HibernateUtil;
+
+public class App2 {
+  public static void main(String... args) {
+    Session session = null;
+    
+    try {
+      session = HibernateUtil.getSession();
+      Comment comment = (Comment) session.get(Comment.class, 1L);
+      System.out.println(comment);
+    } catch (HibernateException ex) {
+      ex.printStackTrace(System.err);
+    }
+  }
+}
