be4d0b2f5d3f3620bce6ec016df956f634c19ef7
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-03 22:10:34 +0600

    changed a DB column name from 'userId' to 'user_id'

--- hibernate-lesson05/src/main/sql/script.sql
+++ hibernate-lesson05/src/main/sql/script.sql
@@ -15,10 +15,10 @@ CREATE TABLE User (
 CREATE TABLE Comment (
   id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   text VARCHAR(500) NOT NULL,
-  userId INTEGER NOT NULL,
-  CONSTRAINT comment_user_fk FOREIGN KEY (userId) REFERENCES User(id)
+  user_id INTEGER NOT NULL,
+  CONSTRAINT comment_user_fk FOREIGN KEY (user_id) REFERENCES User(id)
 );
 
 INSERT INTO User (id, name, createdDate) VALUES (1, 'shamim', '2013-04-03');
-INSERT INTO Comment (text, userId) VALUES ('In God we trust, others must pay cash', 1);
-INSERT INTO Comment (text, userId) VALUES ('There is no place like www.home.com', 1);
+INSERT INTO Comment (text, user_id) VALUES ('In God we trust, others must pay cash', 1);
+INSERT INTO Comment (text, user_id) VALUES ('There is no place like www.home.com', 1);
