83727fc86ab7242233ddb9e8641db59591b5bb58
Shamim Ahmed <shamim.ahmed.freelance@gmail.com>
2013-04-04 13:19:56 +0600

    - added a new class to test user deletion - minor modification in SQL script

---
+++ hibernate-lesson01/src/main/java/edu/buet/cse/hibernate/lesson01/App4.java
@@ -0,0 +1,34 @@
+package edu.buet.cse.hibernate.lesson01;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import edu.buet.cse.hibernate.lesson01.model.User;
+import edu.buet.cse.hibernate.lesson01.util.HibernateUtil;
+
+// example of User deletion
+public class App4 {
+  public static void main(String... args) {
+    Session session = null;
+    
+    try {
+      session = HibernateUtil.getSession();
+      User user = (User) session.get(User.class, 1L);
+      
+      Transaction tx = session.beginTransaction();
+      session.delete(user);
+      tx.commit();
+      
+      System.out.println("User deleted successfully");
+    } catch (HibernateException ex) {
+      ex.printStackTrace(System.err);
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+      
+      HibernateUtil.cleanUp();
+    }
+  }
+}
